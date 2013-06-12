97d9bf7e0599e7ed9fd526e66b16607c6e5a9b38
zhang <zhangshiqi68@gmail.com>
2013-06-12 16:09:56 +0800

    sql

---
+++ driftlibrary.sql
@@ -0,0 +1,251 @@
+CREATE DATABASE  IF NOT EXISTS `driftlibrary` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `driftlibrary`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: driftlibrary
+-- ------------------------------------------------------
+-- Server version	5.5.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `repository`
+--
+
+DROP TABLE IF EXISTS `repository`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repository` (
+  `PersonId` int(9) NOT NULL,
+  `DocId` int(9) DEFAULT NULL,
+  PRIMARY KEY (`PersonId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `repository`
+--
+
+LOCK TABLES `repository` WRITE;
+/*!40000 ALTER TABLE `repository` DISABLE KEYS */;
+/*!40000 ALTER TABLE `repository` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `AuthorId` int(9) NOT NULL AUTO_INCREMENT,
+  `AName` varchar(250) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`AuthorId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'zhang');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrow`
+--
+
+DROP TABLE IF EXISTS `borrow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `borrow` (
+  `BorrowNum` int(11) NOT NULL AUTO_INCREMENT,
+  `BorrowerId` int(9) NOT NULL,
+  `DocId` int(9) NOT NULL,
+  `BorrowDate` datetime NOT NULL,
+  PRIMARY KEY (`BorrowNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrow`
+--
+
+LOCK TABLES `borrow` WRITE;
+/*!40000 ALTER TABLE `borrow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `borrow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `DocId` int(9) NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(250) COLLATE utf8_bin NOT NULL,
+  `AuthorId` int(9) NOT NULL,
+  `Count` int(11) DEFAULT NULL,
+  `Borrowed` int(11) DEFAULT NULL,
+  PRIMARY KEY (`DocId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `PersonId` int(9) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Email` varchar(90) COLLATE utf8_bin DEFAULT NULL,
+  `PassWord` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Address` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Money` int(11) NOT NULL,
+  `Authority` int(11) NOT NULL,
+  PRIMARY KEY (`PersonId`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (3,'cettco','helloworld@gmail.com','111','test',0,0),(4,'cettco','zhangshiqi68@163.com','222','test',0,0),(5,'test','helloworld@gmail.com','111','test',0,0),(6,'zhang','helloworld@gmail.com','','test',0,0),(7,'1','1','2','test',0,0),(8,NULL,NULL,NULL,'test',0,0),(9,NULL,NULL,NULL,'test',0,0),(10,NULL,NULL,NULL,'test',0,0),(11,NULL,NULL,NULL,'test',0,0),(12,NULL,NULL,NULL,'test',0,0),(13,NULL,NULL,NULL,'test',0,0),(14,NULL,NULL,NULL,'test',0,0),(15,NULL,NULL,NULL,'test',0,0),(16,NULL,NULL,NULL,'test',0,0),(17,NULL,NULL,NULL,'test',0,0),(18,'test','test@gmail.com','11111','test',0,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `need`
+--
+
+DROP TABLE IF EXISTS `need`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `need` (
+  `PersonId` int(9) NOT NULL,
+  `Title` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`PersonId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `need`
+--
+
+LOCK TABLES `need` WRITE;
+/*!40000 ALTER TABLE `need` DISABLE KEYS */;
+/*!40000 ALTER TABLE `need` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `idpost` int(9) NOT NULL,
+  `Msg` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `RePId` int(9) NOT NULL,
+  PRIMARY KEY (`idpost`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'hello world',3),(2,'my name is zhang',4);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `document`
+--
+
+DROP TABLE IF EXISTS `document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `document` (
+  `DocId` int(9) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(250) COLLATE utf8_bin NOT NULL,
+  `Description` varchar(1000) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`DocId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document`
+--
+
+LOCK TABLES `document` WRITE;
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `repost`
+--
+
+DROP TABLE IF EXISTS `repost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repost` (
+  `RePId` int(9) NOT NULL,
+  `ReMsg` varchar(1000) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`RePId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `repost`
+--
+
+LOCK TABLES `repost` WRITE;
+/*!40000 ALTER TABLE `repost` DISABLE KEYS */;
+/*!40000 ALTER TABLE `repost` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-06-12 16:04:45
