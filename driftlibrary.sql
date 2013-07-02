d81aed08173f278e27abffe81fcf3c5dec237b32
zhang <zhangshiqi68@gmail.com>
2013-07-02 15:36:26 +0800

    update database

--- driftlibrary.sql
+++ driftlibrary.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `driftlibrary` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-USE `driftlibrary`;
--- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.5.25, for Win64 (x86)
 --
 -- Host: localhost    Database: driftlibrary
 -- ------------------------------------------------------
@@ -16,29 +14,6 @@ USE `driftlibrary`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `repository`
---
-
-DROP TABLE IF EXISTS `repository`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `repository` (
-  `PersonId` int(9) NOT NULL,
-  `DocId` int(9) DEFAULT NULL,
-  PRIMARY KEY (`PersonId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `repository`
---
-
-LOCK TABLES `repository` WRITE;
-/*!40000 ALTER TABLE `repository` DISABLE KEYS */;
-/*!40000 ALTER TABLE `repository` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `author`
@@ -62,6 +37,39 @@ LOCK TABLES `author` WRITE;
 /*!40000 ALTER TABLE `author` DISABLE KEYS */;
 INSERT INTO `author` VALUES (1,'zhang');
 /*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `BookId` int(9) NOT NULL AUTO_INCREMENT,
+  `UserId` int(9) DEFAULT NULL,
+  `Title` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Subtitle` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Author` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Image` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Author_intro` text COLLATE utf8_bin,
+  `Summary` text COLLATE utf8_bin,
+  `Publisher` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `Pubdate` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`BookId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(3,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(5,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(6,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(7,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -90,58 +98,27 @@ LOCK TABLES `borrow` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `book`
+-- Table structure for table `document`
 --
 
-DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `document`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `book` (
+CREATE TABLE `document` (
   `DocId` int(9) NOT NULL AUTO_INCREMENT,
-  `ISBN` varchar(250) COLLATE utf8_bin NOT NULL,
-  `AuthorId` int(9) NOT NULL,
-  `Count` int(11) DEFAULT NULL,
-  `Borrowed` int(11) DEFAULT NULL,
+  `Title` varchar(250) COLLATE utf8_bin NOT NULL,
+  `Description` varchar(1000) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`DocId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `book`
+-- Dumping data for table `document`
 --
 
-LOCK TABLES `book` WRITE;
-/*!40000 ALTER TABLE `book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `PersonId` int(9) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `Email` varchar(90) COLLATE utf8_bin DEFAULT NULL,
-  `PassWord` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `Address` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `Money` int(11) NOT NULL,
-  `Authority` int(11) NOT NULL,
-  PRIMARY KEY (`PersonId`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (3,'cettco','helloworld@gmail.com','111','test',0,0),(4,'cettco','zhangshiqi68@163.com','222','test',0,0),(5,'test','helloworld@gmail.com','111','test',0,0),(6,'zhang','helloworld@gmail.com','','test',0,0),(7,'1','1','2','test',0,0),(8,NULL,NULL,NULL,'test',0,0),(9,NULL,NULL,NULL,'test',0,0),(10,NULL,NULL,NULL,'test',0,0),(11,NULL,NULL,NULL,'test',0,0),(12,NULL,NULL,NULL,'test',0,0),(13,NULL,NULL,NULL,'test',0,0),(14,NULL,NULL,NULL,'test',0,0),(15,NULL,NULL,NULL,'test',0,0),(16,NULL,NULL,NULL,'test',0,0),(17,NULL,NULL,NULL,'test',0,0),(18,'test','test@gmail.com','11111','test',0,0);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `document` WRITE;
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -193,27 +170,26 @@ INSERT INTO `post` VALUES (1,'hello world',3),(2,'my name is zhang',4);
 UNLOCK TABLES;
 
 --
--- Table structure for table `document`
+-- Table structure for table `repository`
 --
 
-DROP TABLE IF EXISTS `document`;
+DROP TABLE IF EXISTS `repository`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `document` (
-  `DocId` int(9) NOT NULL AUTO_INCREMENT,
-  `Title` varchar(250) COLLATE utf8_bin NOT NULL,
-  `Description` varchar(1000) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`DocId`)
+CREATE TABLE `repository` (
+  `PersonId` int(9) NOT NULL,
+  `DocId` int(9) DEFAULT NULL,
+  PRIMARY KEY (`PersonId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `document`
+-- Dumping data for table `repository`
 --
 
-LOCK TABLES `document` WRITE;
-/*!40000 ALTER TABLE `document` DISABLE KEYS */;
-/*!40000 ALTER TABLE `document` ENABLE KEYS */;
+LOCK TABLES `repository` WRITE;
+/*!40000 ALTER TABLE `repository` DISABLE KEYS */;
+/*!40000 ALTER TABLE `repository` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -238,6 +214,59 @@ LOCK TABLES `repost` WRITE;
 /*!40000 ALTER TABLE `repost` DISABLE KEYS */;
 /*!40000 ALTER TABLE `repost` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `idTag` int(11) NOT NULL,
+  `BookId` int(9) DEFAULT NULL,
+  `Tag` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idTag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `UserId` int(9) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Email` varchar(90) COLLATE utf8_bin DEFAULT NULL,
+  `PassWord` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Address` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `Money` int(11) NOT NULL,
+  `Authority` int(11) NOT NULL,
+  PRIMARY KEY (`UserId`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (3,'cettco','helloworld@gmail.com','111','test',0,0),(4,'cettco','testabc@123.com','222','test',0,0),(5,'test','helloworld@gmail.com','111','test',0,0),(6,'zhang','helloworld@gmail.com','','test',0,0),(7,'1','1','2','test',0,0),(8,NULL,NULL,NULL,'test',0,0),(9,NULL,NULL,NULL,'test',0,0),(10,NULL,NULL,NULL,'test',0,0),(11,NULL,NULL,NULL,'test',0,0),(12,NULL,NULL,NULL,'test',0,0),(13,NULL,NULL,NULL,'test',0,0),(14,NULL,NULL,NULL,'test',0,0),(15,NULL,NULL,NULL,'test',0,0),(16,NULL,NULL,NULL,'test',0,0),(17,NULL,NULL,NULL,'test',0,0),(18,'test','test@gmail.com','11111','test',0,0),(19,'abc','test@163.com','zsqzsq','test',0,0),(20,'qq','qq@qq.com','111111','test',0,0),(21,'hahah','111@11.com','qqqqq','test',0,0),(22,'abc','aaa@g.com','11111','test',0,0),(23,'dsaf','sdaf@1.com','11111','test',0,0),(24,'111','111@11.co','22222','test',0,0),(25,'111','111@11.c','22222','test',0,0),(26,'11','22132@1.C','11111','test',0,0),(27,'dsaf','adf@111.com','3333333','test',0,0),(28,'te','tes@gmail.com','22222','test',0,0),(29,'dsaf','asdf@11.v','11111','test',0,0),(30,'dsafas','adsfasd@111.com','11111','test',0,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -248,13 +277,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-06-12 16:04:45
-declare @a int,@b int
-set @a=1
-set @b=1000
-while @a<40
-begin
-   insert into book (ISBN) values(@b)
-   set @a=@a+1
-   set @b=@b+2
-end
+-- Dump completed on 2013-07-02 15:33:52
